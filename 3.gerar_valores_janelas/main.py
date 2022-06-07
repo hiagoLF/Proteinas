4043214ee03d3ab6ad3e8a009eb9548afca96676
hiagoLF <hiagolof@gmail.com>
2022-06-07 18:05:07 -0300

    gerar_valores_janelas

--- 3.gerar_valores_janelas/main.py
+++ 3.gerar_valores_janelas/main.py
@@ -5,7 +5,7 @@
 # Guardas valores em tabelas
 
 import pandas as pd
-import os 
+import os
 from numeros.precisao import diminuirPrecisaoDeNumeros
 from gerar.dicionario import gerarDicionarioDoDataFrame
 from score.calcularScoresDeSequencias import calcularScoresDasSequencias
@@ -14,33 +14,40 @@ import matplotlib.pyplot as plt
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-pontuacoesDeAminoAcidos = pd.read_csv(dir_path+'/../input/razoes_aminoacidos.csv')
-pontuacoesDeAminoAcidosDuplas = pd.read_csv(dir_path+'/../input/razoes_duplas_aminoacidos.csv', keep_default_na=False)
+pontuacoesDeAminoAcidos = pd.read_csv(
+    dir_path+'/../input/razoes_aminoacidos.csv')
+pontuacoesDeAminoAcidosDuplas = pd.read_csv(
+    dir_path+'/../input/razoes_duplas_aminoacidos.csv', keep_default_na=False)
 
-pontuacoesDeAminoAcidosDicionario = gerarDicionarioDoDataFrame(pontuacoesDeAminoAcidos, 'AMINO_ACID', 'REASON')
-pontuacoesDeAminoAcidosDuplasDicionario = gerarDicionarioDoDataFrame(pontuacoesDeAminoAcidosDuplas, 'AMINO_ACID_COUPLE', 'REASON')
+pontuacoesDeAminoAcidosDicionario = gerarDicionarioDoDataFrame(
+    pontuacoesDeAminoAcidos, 'AMINO_ACID', 'REASON')
+pontuacoesDeAminoAcidosDuplasDicionario = gerarDicionarioDoDataFrame(
+    pontuacoesDeAminoAcidosDuplas, 'AMINO_ACID_COUPLE', 'REASON')
 
-sequenciasTransmembrana = pd.read_csv(dir_path+'/../output/fragmentos_transmembrana.csv')
-sequenciasNaoTransmembrana = pd.read_csv(dir_path+'/../output/fragmentos_nao_transmembrana.csv', keep_default_na=False)
+sequenciasTransmembrana = pd.read_csv(
+    dir_path+'/../output/fragmentos_transmembrana.csv')
+sequenciasNaoTransmembrana = pd.read_csv(
+    dir_path+'/../output/fragmentos_nao_transmembrana.csv', keep_default_na=False)
 
 scoresSequenciasTransmembrana = calcularScoresDasSequencias(
-    sequenciasTransmembrana['SEQUENCIA_TRANSMEMBRANA'], 
-    pontuacoesDeAminoAcidosDicionario, pontuacoesDeAminoAcidosDuplasDicionario, 
+    sequenciasTransmembrana['SEQUENCIA_TRANSMEMBRANA'],
+    pontuacoesDeAminoAcidosDicionario, pontuacoesDeAminoAcidosDuplasDicionario,
 )
 scoresSequenciasNaoTransmembrana = calcularScoresDasSequencias(
-    sequenciasNaoTransmembrana['SEQUENCIA_NAO_TRANSMEMBRANA'], 
-    pontuacoesDeAminoAcidosDicionario, pontuacoesDeAminoAcidosDuplasDicionario, 
+    sequenciasNaoTransmembrana['SEQUENCIA_NAO_TRANSMEMBRANA'],
+    pontuacoesDeAminoAcidosDicionario, pontuacoesDeAminoAcidosDuplasDicionario,
 )
 
 diminuirPrecisaoDeNumeros(scoresSequenciasTransmembrana)
 diminuirPrecisaoDeNumeros(scoresSequenciasNaoTransmembrana)
+
 
 resumoScoresTransMembrana = pd.DataFrame(scoresSequenciasTransmembrana)
 resumoScoresTransMembrana.plot.kde()
 plt.title('Distribuição dos scores em janelas transmembrana')
 plt.xlabel("Score")
 plt.ylabel("Densidade")
-plt.savefig(dir_path+'/../output/distr_transmembrana.png')
+plt.savefig(dir_path+'/../output/valores_janelas/distr_transmembrana.png')
 
 
 plt.clf()
@@ -49,4 +56,13 @@ resumoScoresNaoTransMembrana.plot.kde()
 plt.title('Distribuição dos scores em janelas não transmembrana')
 plt.xlabel("Score")
 plt.ylabel("Densidade")
-plt.savefig(dir_path+'/../output/distr_nao_transmembrana.png')
+plt.savefig(dir_path+'/../output/valores_janelas/distr_nao_transmembrana.png')
+
+
+dfScoresTransmembrana = pd.DataFrame(
+    {'SCORE_TRANSMEMBRANA': scoresSequenciasTransmembrana})
+dfScoresNaoTransmembrana = pd.DataFrame(
+    {'SCORE_NAO_TRANSMEMBRANA': scoresSequenciasNaoTransmembrana})
+
+dfScoresTransmembrana.to_csv(dir_path+'/../output/valores_janelas/scores_sec_transmembrana.csv')
+dfScoresNaoTransmembrana.to_csv(dir_path+'/../output/valores_janelas/scores_sec_nao_transmembrana.csv')
